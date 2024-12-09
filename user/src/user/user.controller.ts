c462dac6f73f33016a2da2c3c909aad2b4222b0c
DmytroKondrashov <kondrashovd101@gmail.com>
2024-12-09 15:41:07 +0200

    add basic actions to user controller

--- user/src/user/user.controller.ts
+++ user/src/user/user.controller.ts
@@ -1,4 +1,28 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { UserService } from './user.service';
+import { User } from './entities/user.entity';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Post()
+  create(@Body() user: User) {
+    return this.userService.create(user);
+  }
+
+  @Get()
+  findAll() {
+    return this.userService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string, @Param('email') email: string) {
+    return this.userService.findOne({ id: Number(id), email });
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string) {
+    return this.userService.delete(Number(id));
+  }
+}
