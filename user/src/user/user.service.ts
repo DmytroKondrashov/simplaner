c9f1b054c54960bb5961d3d239485aec60b17d11
DmytroKondrashov <kondrashovd101@gmail.com>
2024-12-09 11:50:28 +0200

    add basic user actions

--- user/src/user/user.service.ts
+++ user/src/user/user.service.ts
@@ -1,4 +1,38 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { DeleteResult, Repository } from 'typeorm';
+import { SearchUserDto } from './dto/search-user.dto';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
+
+  async create(user: Partial<User>): Promise<User> {
+    const newUser = this.userRepository.create(user);
+    return this.userRepository.save(newUser);
+  }
+
+  async findAll(): Promise<User[]> {
+    return this.userRepository.find();
+  }
+
+  async findOne(data: SearchUserDto) {
+    const { id, email } = data;
+    const query = this.userRepository.createQueryBuilder('user');
+    if (id) {
+      query.where('user.id = :id', { id });
+    }
+    if (email) {
+      query.where('user.email = :email', { email });
+    }
+    return query.getOne();
+  }
+
+  async delete(id: number): Promise<DeleteResult> {
+    return this.userRepository.delete(id);
+  }
+}
