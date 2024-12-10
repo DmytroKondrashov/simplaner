4a7a14485b2a4a8d37f6c72c71513c59a40d2695
DmytroKondrashov <kondrashovd101@gmail.com>
2024-12-10 12:52:31 +0200

    set up the basic Kafka service

--- api-gateway/src/user/user.service.ts
+++ api-gateway/src/user/user.service.ts
@@ -1,4 +1,23 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { Kafka, Producer, ProducerRecord } from 'kafkajs';
 
 @Injectable()
-export class UserService {}
+export class UserService implements OnModuleInit, OnModuleDestroy {
+  private readonly kafka = new Kafka({
+    brokers: [process.env.KAFKA_BROKER],
+  });
+
+  private readonly producer: Producer = this.kafka.producer();
+
+  async onModuleInit() {
+    await this.producer.connect();
+  }
+
+  async onModuleDestroy() {
+    await this.producer.disconnect();
+  }
+
+  async produce(record: ProducerRecord) {
+    await this.producer.send(record);
+  }
+}
