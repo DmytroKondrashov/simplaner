ab205e10a196ed4ce34c549307d93db03bab9379
DmytroKondrashov <kondrashovd101@gmail.com>
2024-12-11 11:34:37 +0200

    update recort argument type

--- api-gateway/src/user/user.controller.ts
+++ api-gateway/src/user/user.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { UserService } from './user.service';
+import { CreateUserDto } from './dtos/create-user.dto';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Post('create')
+  async create(@Body() body: CreateUserDto) {
+    return this.userService.produce(body, 'user.created');
+  }
+}
