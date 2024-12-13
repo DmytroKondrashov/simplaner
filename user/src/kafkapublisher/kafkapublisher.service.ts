4158b59622acfe7e74a2fd9218087b2ae859ee7e
DmytroKondrashov <kondrashovd101@gmail.com>
2024-12-13 17:59:33 +0200

    finish kafka publisher

--- user/src/kafkapublisher/kafkapublisher.service.ts
+++ user/src/kafkapublisher/kafkapublisher.service.ts
@@ -1,4 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { Kafka, Producer } from 'kafkajs';
 
 @Injectable()
-export class KafkapublisherService {}
+export class KafkapublisherService implements OnModuleInit, OnModuleDestroy {
+  private readonly kafka = new Kafka({
+    brokers: [process.env.KAFKA_BROKER],
+  });
+
+  private readonly producer: Producer = this.kafka.producer();
+
+  async onModuleInit() {
+    await this.producer.connect();
+  }
+
+  async onModuleDestroy() {
+    await this.producer.disconnect();
+  }
+
+  async produce(record: any, topic: string) {
+    await this.producer.send({
+      topic,
+      messages: [{ value: JSON.stringify(record) }],
+    });
+  }
+}
