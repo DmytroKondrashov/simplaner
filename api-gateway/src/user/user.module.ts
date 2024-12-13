037946bbf020372ded0cf59c77cce949dfeaa370
DmytroKondrashov <kondrashovd101@gmail.com>
2024-12-13 18:56:56 +0200

    fix imports

--- api-gateway/src/user/user.module.ts
+++ api-gateway/src/user/user.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
+import { PublisherService } from 'src/publisher/publisher.service';
 
 @Module({
   controllers: [UserController],
-  providers: [UserService]
+  providers: [UserService, PublisherService],
 })
 export class UserModule {}
