b11c93348b7cf40988d97e7da1b25ef4accf8d05
DmytroKondrashov <kondrashovd101@gmail.com>
2024-12-14 19:25:46 +0200

    add consumer service

--- api-gateway/src/app.module.ts
+++ api-gateway/src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { PublisherService } from './publisher/publisher.service';
+import { ConsumerService } from './consumer/consumer.service';
 
 @Module({
   imports: [UserModule],
   controllers: [AppController],
-  providers: [AppService, PublisherService],
+  providers: [AppService, PublisherService, ConsumerService],
 })
 export class AppModule {}
