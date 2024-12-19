8f28e1a413dd1d7d4ad61b1d5932b497d94001a0
DmytroKondrashov <kondrashovd101@gmail.com>
2024-12-19 14:51:18 +0200

    register the microservice

--- user/src/main.ts
+++ user/src/main.ts
@@ -2,12 +2,23 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as dotenv from 'dotenv';
+import { Transport } from '@nestjs/microservices';
 
 dotenv.config();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.createMicroservice(AppModule, {
+    transport: Transport.KAFKA,
+    options: {
+      client: {
+        brokers: ['localhost:9092'],
+      },
+      consumer: {
+        groupId: 'user-consumer',
+      },
+    },
+  });
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(process.env.PORT ?? 3002);
+  await app.listen();
 }
 bootstrap();
