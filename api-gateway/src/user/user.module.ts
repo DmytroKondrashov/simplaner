6a855ebc2da82d4b2ad8ff03a864e08c29d513a0
DmytroKondrashov <kondrashovd101@gmail.com>
2024-12-19 14:32:48 +0200

    register the USER_SERVICE

--- api-gateway/src/user/user.module.ts
+++ api-gateway/src/user/user.module.ts
@@ -2,8 +2,25 @@ import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 import { PublisherService } from 'src/publisher/publisher.service';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'USER_SERVICE',
+        transport: Transport.KAFKA,
+        options: {
+          client: {
+            brokers: ['localhost:9092'],
+          },
+          consumer: {
+            groupId: 'api-gateway-consumer',
+          },
+        },
+      },
+    ]),
+  ],
   controllers: [UserController],
   providers: [UserService, PublisherService],
 })
