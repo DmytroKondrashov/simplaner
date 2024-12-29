7783650e2363b40c64a8885f0793b60fe0add608
DmytroKondrashov <kondrashovd101@gmail.com>
2024-12-29 13:23:51 +0200

    configure passport jvt srategy

--- user/src/user/user.module.ts
+++ user/src/user/user.module.ts
@@ -4,10 +4,18 @@ import { UserService } from './user.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { ClientsModule, Transport } from '@nestjs/microservices';
+import { PassportModule } from '@nestjs/passport';
+import { JwtStrategy } from './jwt.strategy';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([User]),
+    PassportModule,
+    JwtModule.register({
+      secret: 'df;kb;lvjeorpjriuvriohvnroiurgsavvdv',
+      signOptions: { expiresIn: '1h' },
+    }),
     ClientsModule.register([
       {
         name: 'API_GATEWAY_SERVICE',
@@ -23,7 +31,7 @@ import { ClientsModule, Transport } from '@nestjs/microservices';
       },
     ]),
   ],
-  controllers: [UserController],
+  controllers: [UserController, JwtStrategy],
   providers: [UserService],
 })
 export class UserModule {}
