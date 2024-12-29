35e787247af3caeef31379373595a9633d78b6c6
DmytroKondrashov <kondrashovd101@gmail.com>
2024-12-29 21:51:16 +0200

    add rpc exception to auth guard

--- user/src/guards/auth.guard.ts
+++ user/src/guards/auth.guard.ts
@@ -1,5 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { RpcException } from '@nestjs/microservices';
 
 @Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt') {}
+export class JwtAuthGuard extends AuthGuard('jwt') {
+  handleRequest(err: any, user: any) {
+    if (err || !user) {
+      throw new RpcException({
+        statusCode: 401,
+        message: 'Unauthorized',
+      });
+    }
+    return user;
+  }
+}
