3423ae8452819a7a13afe835a5a023c5a0601899
DmytroKondrashov <kondrashovd101@gmail.com>
2025-01-03 20:01:44 +0200

    make auth microservice

--- auth/src/main.ts
+++ auth/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.createMicroservice(AppModule, {
+    transport: Transport.KAFKA,
+    options: {
+      client: {
+        clientId: 'auth',
+        brokers: ['localhost:9092'],
+      },
+      consumer: {
+        groupId: 'auth-consumer',
+      },
+    },
+  });
+  await app.listen();
 }
 bootstrap();
