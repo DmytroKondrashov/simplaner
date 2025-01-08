86d46a759727eb75c561c952c0e5644be09d994b
DmytroKondrashov <kondrashovd101@gmail.com>
2025-01-08 22:08:26 +0200

    add LIST_SERVICE

--- api-gateway/src/list/list.module.ts
+++ api-gateway/src/list/list.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
 import { ListController } from './list.controller';
 import { ListService } from './list.service';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'LIST_SERVICE',
+        transport: Transport.KAFKA,
+        options: {
+          client: {
+            clientId: 'list',
+            brokers: ['localhost:9092'],
+          },
+          consumer: {
+            groupId: 'list-consumer',
+          },
+        },
+      },
+    ]),
+  ],
   controllers: [ListController],
-  providers: [ListService]
+  providers: [ListService],
 })
 export class ListModule {}
