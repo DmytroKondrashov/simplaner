5ab18556ffa771552b05ee89e4ff67253e7af7d3
DmytroKondrashov <kondrashovd101@gmail.com>
2025-01-28 21:52:31 +0200

    configure main

--- list/src/main.ts
+++ list/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.createMicroservice(AppModule, {
+    transport: Transport.KAFKA,
+    options: {
+      client: {
+        brokers: ['localhost:9092'],
+      },
+      consumer: {
+        groupId: 'list-consumer',
+      },
+    },
+  });
+  await app.listen();
 }
 bootstrap();
