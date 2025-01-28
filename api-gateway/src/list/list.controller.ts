992d7411f67cdbd6d40fc3cb7f3614a4a5507810
DmytroKondrashov <kondrashovd101@gmail.com>
2025-01-28 21:40:14 +0200

    add basic list controller and service

--- api-gateway/src/list/list.controller.ts
+++ api-gateway/src/list/list.controller.ts
@@ -1,4 +1,42 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Headers,
+  Param,
+  Post,
+} from '@nestjs/common';
+import { ListService } from './list.service';
+import { CreateListDto } from './dtos/create.list.dto';
+import { UpdateListDto } from './dtos/update.post.dto';
 
 @Controller('list')
-export class ListController {}
+export class ListController {
+  constructor(private readonly listService: ListService) {}
+
+  @Get('all')
+  async findAll(@Headers('Authorization') authHeader: string) {
+    const token = authHeader.replace('Bearer ', '');
+    return this.listService.findAll(token);
+  }
+
+  @Post('create')
+  async create(@Body() body: CreateListDto) {
+    return this.listService.create(body);
+  }
+
+  @Post('update')
+  async update(@Body() body: UpdateListDto) {
+    return this.listService.update(body);
+  }
+
+  @Delete(':id')
+  async delete(
+    @Param('id') id: string,
+    @Headers('Authorization') authHeader: string,
+  ) {
+    const token = authHeader.replace('Bearer ', '');
+    return this.listService.delete(id, token);
+  }
+}
--- api-gateway/src/list/list.service.ts
+++ api-gateway/src/list/list.service.ts
@@ -1,4 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
+import { CreateListDto } from './dtos/create.list.dto';
+import { UpdateListDto } from './dtos/update.post.dto';
 
 @Injectable()
-export class ListService {}
+export class ListService {
+  constructor(private readonly client: ClientProxy) {}
+
+  async findAll(token: string) {
+    return this.client.send('list.findAll', { token });
+  }
+
+  async create(body: CreateListDto) {
+    return this.client.send('list.create', { body });
+  }
+
+  async update(body: UpdateListDto) {
+    return this.client.send('list.update', { body });
+  }
+
+  async delete(id: string, token: string) {
+    return this.client.send('list.delete', { id, token });
+  }
+}
