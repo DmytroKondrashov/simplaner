3f6eb1f6aa2eb09d476cc39fe63c253684fce0e0
DmytroKondrashov <kondrashovd101@gmail.com>
2025-01-28 21:54:03 +0200

    add basic controller

--- list/src/app.controller.ts
+++ list/src/app.controller.ts
@@ -1,12 +1,28 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { AppService } from './app.service';
+import { MessagePattern } from '@nestjs/microservices';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @MessagePattern('list.findAll')
+  async findAll(token: string) {
+    return this.appService.findAll(token);
+  }
+
+  @MessagePattern('list.create')
+  async create(body: CreateListDto) {
+    return this.appService.create(body);
+  }
+
+  @MessagePattern('list.update')
+  async update(body: UpdateListDto) {
+    return this.appService.update(body);
+  }
+
+  @MessagePattern('list.delete')
+  async delete(id: string, token: string) {
+    return this.appService.delete(id, token);
   }
 }
