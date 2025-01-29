43bf5b2ad87d9057f2ca2e0a4bf2f66375f99fdd
DmytroKondrashov <kondrashovd101@gmail.com>
2025-01-29 12:56:41 +0200

    config the app module

--- list/src/app.module.ts
+++ list/src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { List } from './entities/list.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forFeature([List]),
+    ClientsModule.register([
+      {
+        name: 'API_GATEWAY_SERVICE',
+        transport: Transport.KAFKA,
+        options: {
+          client: {
+            brokers: ['localhost:9092'],
+          },
+        },
+      },
+    ]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
