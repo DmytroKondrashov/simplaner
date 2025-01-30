6981eabf0ab141dc67da6460068857a1cfe596c8
DmytroKondrashov <kondrashovd101@gmail.com>
2025-01-30 16:33:20 +0200

    finish basic service

--- list/src/app.service.ts
+++ list/src/app.service.ts
@@ -1,8 +1,48 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable, OnModuleInit } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { List } from './entities/list.entity';
+import { Repository } from 'typeorm';
+import { ClientKafka } from '@nestjs/microservices';
+import { JwtService } from '@nestjs/jwt';
+import { CreateListDto } from './dtos/create.list.dto';
+import { UpdateListDto } from './dtos/update.list.dto';
 
 @Injectable()
-export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+export class AppService implements OnModuleInit {
+  constructor(
+    private readonly jwtService: JwtService,
+    @InjectRepository(List)
+    private readonly listRepository: Repository<List>,
+    @Inject('API_GATEWAY_SERVICE') private readonly client: ClientKafka,
+  ) {}
+
+  onModuleInit() {
+    this.client.subscribeToResponseOf('list.findAll');
+    this.client.subscribeToResponseOf('list.create');
+    this.client.subscribeToResponseOf('list.update');
+    this.client.subscribeToResponseOf('list.delete');
+  }
+
+  async findAll(token: string) {
+    const decodedToken = await this.jwtService.verifyAsync(token);
+    return this.listRepository.find({ where: { userId: decodedToken.id } });
+  }
+
+  async create(body: CreateListDto, token: string) {
+    const decodedToken = await this.jwtService.verifyAsync(token);
+    return this.listRepository.save({ ...body, userId: decodedToken.id });
+  }
+
+  async update(body: UpdateListDto, token: string) {
+    const decodedToken = await this.jwtService.verifyAsync(token);
+    return this.listRepository.update(body.id, {
+      ...body,
+      userId: Number(decodedToken.id),
+    });
+  }
+
+  async delete(id: number, token: string) {
+    const decodedToken = await this.jwtService.verifyAsync(token);
+    return this.listRepository.delete({ id, userId: Number(decodedToken.id) });
   }
 }
