c663dbda72f839fcf8829608482f9252697e68f7
DmytroKondrashov <kondrashovd101@gmail.com>
2025-01-31 18:38:36 +0200

    add basic controller

--- item/src/app.controller.ts
+++ item/src/app.controller.ts
@@ -1,12 +1,29 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Item } from './entities/item.entity';
+import { MessagePattern } from '@nestjs/microservices';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @MessagePattern('item.create')
+  createItem(item: Item): Promise<Item> {
+    return this.appService.createItem(item);
+  }
+
+  @MessagePattern('item.get')
+  getItem(id: number): Promise<Item> {
+    return this.appService.getItem(id);
+  }
+
+  @MessagePattern('item.update')
+  updateItem(item: Item, id: number): Promise<Item> {
+    return this.appService.updateItem(id, item);
+  }
+
+  @MessagePattern('item.delete')
+  deleteItem(id: number): Promise<void> {
+    return this.appService.deleteItem(id);
   }
 }
