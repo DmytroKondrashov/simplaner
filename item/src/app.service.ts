e02ee8a7d6163159d00ce79981d66207a3da9bb9
DmytroKondrashov <kondrashovd101@gmail.com>
2025-01-31 13:24:17 +0200

    add basic service

--- item/src/app.service.ts
+++ item/src/app.service.ts
@@ -1,8 +1,33 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Item } from './entities/item.entity';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(
+    @InjectRepository(Item)
+    private readonly itemRepository: Repository<Item>,
+  ) {}
+
+  async createItem(item: Item): Promise<Item> {
+    return this.itemRepository.save(item);
+  }
+
+  async getItems(): Promise<Item[]> {
+    return this.itemRepository.find();
+  }
+
+  async getItem(id: number): Promise<Item> {
+    return this.itemRepository.findOne({ where: { id } });
+  }
+
+  async updateItem(id: number, item: Item): Promise<Item> {
+    await this.itemRepository.update(id, item);
+    return this.itemRepository.findOne({ where: { id } });
+  }
+
+  async deleteItem(id: number): Promise<void> {
+    await this.itemRepository.delete(id);
   }
 }
