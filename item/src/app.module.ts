e8d58e36b8df83b36e139ae78f77607434d940a8
DmytroKondrashov <kondrashovd101@gmail.com>
2025-01-31 13:18:16 +0200

    add list typeorm feature

--- item/src/app.module.ts
+++ item/src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { Item } from './entities/item.entity';
 
 @Module({
   imports: [
@@ -18,6 +19,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
       }),
       inject: [ConfigService],
     }),
+    TypeOrmModule.forFeature([Item]),
   ],
   controllers: [AppController],
   providers: [AppService],
