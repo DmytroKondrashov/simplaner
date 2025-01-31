a956b6636252c48d454183669ee64524b3fa297b
DmytroKondrashov <kondrashovd101@gmail.com>
2025-01-31 12:51:19 +0200

    configure main.ts

--- item/src/main.ts
+++ item/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Transport } from '@nestjs/microservices';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.createMicroservice(AppModule, {
+    transport: Transport.KAFKA,
+    options: {
+      client: {
+        brokers: ['localhost:9092'],
+      },
+      consumer: {
+        groupId: 'item-consumer',
+      },
+    },
+  });
+  await app.listen();
 }
 bootstrap();
