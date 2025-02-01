ec2135a132d1680343568ae61d6da860c16f337e
DmytroKondrashov <kondrashovd101@gmail.com>
2025-02-01 21:55:45 +0200

    create item table migration

---
+++ item/src/1738439716080-migrations.ts
@@ -0,0 +1,15 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class Migrations1738439716080 implements MigrationInterface {
+  name = 'Migrations1738439716080';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE TABLE "item" ("id" SERIAL NOT NULL, "name" character varying NOT NULL, "description" character varying NOT NULL, "listId" integer NOT NULL, CONSTRAINT "PK_d3c0c71f23e7adcf952a1d13423" PRIMARY KEY ("id"))`,
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`DROP TABLE "item"`);
+  }
+}
