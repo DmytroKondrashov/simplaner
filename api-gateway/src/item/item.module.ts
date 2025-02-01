2399b77bd3756291494dd6ba4071f6760d82af02
DmytroKondrashov <kondrashovd101@gmail.com>
2025-02-01 19:32:26 +0200

    confgure the module

--- api-gateway/src/item/item.module.ts
+++ api-gateway/src/item/item.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
 import { ItemController } from './item.controller';
 import { ItemService } from './item.service';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'ITEM_SERVICE',
+        transport: Transport.KAFKA,
+        options: {
+          client: {
+            brokers: ['localhost:9092'],
+          },
+          consumer: {
+            groupId: 'api-gateway-consumer',
+          },
+        },
+      },
+    ]),
+  ],
   controllers: [ItemController],
-  providers: [ItemService]
+  providers: [ItemService],
 })
 export class ItemModule {}
