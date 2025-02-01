c0ee47d2d666b457759519fdb485455552a53917
DmytroKondrashov <kondrashovd101@gmail.com>
2025-02-01 19:48:22 +0200

    add basic item service

--- api-gateway/src/item/item.service.ts
+++ api-gateway/src/item/item.service.ts
@@ -1,4 +1,31 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { ClientKafka } from '@nestjs/microservices';
+import { CreateItemDto } from './dtos/create.item.dto';
+import { UpdateItemDto } from './dtos/update.item.dto';
 
 @Injectable()
-export class ItemService {}
+export class ItemService {
+  constructor(@Inject('ITEM_SERVICE') private readonly client: ClientKafka) {}
+
+  async createItem(payload: CreateItemDto) {
+    return this.client.send('item.create', payload);
+  }
+
+  async updateItem(payload: UpdateItemDto) {
+    return this.client.send('item.update', payload);
+  }
+
+  async findAll(token: string) {
+    return this.client.send('item.findAll', {
+      token,
+    });
+  }
+
+  async findOne(id: string, token: string) {
+    return this.client.send('item.findOne', { id, token });
+  }
+
+  async deleteItem(id: string, token: string) {
+    return this.client.send('item.delete', { id, token });
+  }
+}
