46e2c0ed81272c14c887996eec38d84291b8083f
DmytroKondrashov <kondrashovd101@gmail.com>
2025-02-02 13:36:07 +0200

    add try catch

--- item/src/app.service.ts
+++ item/src/app.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Item } from './entities/item.entity';
+import { RpcException } from '@nestjs/microservices';
 
 @Injectable()
 export class AppService {
@@ -10,8 +11,13 @@ export class AppService {
     private readonly itemRepository: Repository<Item>,
   ) {}
 
-  async createItem(item: Item): Promise<Item> {
-    return this.itemRepository.save(item);
+  async createItem(item: any, token: string): Promise<Item> {
+    try {
+      const result = await this.itemRepository.save(item);
+      return result;
+    } catch (error) {
+      throw new RpcException(error);
+    }
   }
 
   async getItems(): Promise<Item[]> {
