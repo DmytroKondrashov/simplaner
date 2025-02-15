3db3a952acb0653f48fa28b7a048f0c96c573c3d
DmytroKondrashov <kondrashovd101@gmail.com>
2025-02-15 20:08:12 +0200

    remove not null constraint from description

--- item/src/migrations/1738439716080-migrations.ts
+++ item/src/migrations/1738439716080-migrations.ts
@@ -5,7 +5,7 @@ export class Migrations1738439716080 implements MigrationInterface {
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `CREATE TABLE "item" ("id" SERIAL NOT NULL, "name" character varying NOT NULL, "description" character varying NOT NULL, "listId" integer NOT NULL, CONSTRAINT "PK_d3c0c71f23e7adcf952a1d13423" PRIMARY KEY ("id"))`,
+      `CREATE TABLE "item" ("id" SERIAL NOT NULL, "name" character varying NOT NULL, "description" character varying, "listId" integer NOT NULL, CONSTRAINT "PK_d3c0c71f23e7adcf952a1d13423" PRIMARY KEY ("id"))`,
     );
   }
 
