8a36dd83fb11bd90bcaf2493001464efe5437a5b
DmytroKondrashov <kondrashovd101@gmail.com>
2025-02-23 13:02:51 +0200

    remove unneeded connection

--- api-gateway/src/app.module.ts
+++ api-gateway/src/app.module.ts
@@ -2,30 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
-import { ClientsModule, Transport } from '@nestjs/microservices';
 import { ListModule } from './list/list.module';
 import { ItemModule } from './item/item.module';
 
 @Module({
-  imports: [
-    UserModule,
-    ClientsModule.register([
-      {
-        name: 'USER_SERVICE',
-        transport: Transport.KAFKA,
-        options: {
-          client: {
-            brokers: ['localhost:9092'],
-          },
-          consumer: {
-            groupId: 'api-gateway-consumer',
-          },
-        },
-      },
-    ]),
-    ListModule,
-    ItemModule,
-  ],
+  imports: [UserModule, ListModule, ItemModule],
   controllers: [AppController],
   providers: [AppService],
 })
